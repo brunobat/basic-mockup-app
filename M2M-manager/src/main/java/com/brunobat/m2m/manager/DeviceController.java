5cced21376c70e38424469a85309e69defcb94c0
Bruno Baptista <brunobat@gmail.com>
2016-12-15 01:16:54 +0000

    controller logic

--- M2M-manager/src/main/java/com/brunobat/m2m/manager/DeviceController.java
+++ M2M-manager/src/main/java/com/brunobat/m2m/manager/DeviceController.java
@@ -2,6 +2,8 @@ package com.brunobat.m2m.manager;
 
 import com.brunobat.m2m.sensors.DevicesFactory;
 
+import java.util.Map;
+
 /**
  * Created by Bruno Baptista on 15/12/16.
  */
@@ -12,5 +14,11 @@ public class DeviceController {
     public DeviceController() {
         manager = new ResourceManager();
         manager.addDevice(DevicesFactory.getDevices());
+    }
+
+    public void printDeviceStatus() {
+        for (Map.Entry<String, String> entry : manager.getReadingFromAllDevices().entrySet()) {
+            System.out.print("Device: " + entry.getKey() + " reads: " + entry.getValue());
+        }
     }
 }
