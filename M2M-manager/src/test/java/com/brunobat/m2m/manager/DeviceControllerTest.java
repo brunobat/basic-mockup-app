dc3e576fc9c394083ebf78c51a9d9e8783765833
Bruno Baptista <brunobat@gmail.com>
2016-12-15 14:48:56 +0000

    test the resource manager

--- M2M-manager/src/test/java/com/brunobat/m2m/manager/DeviceControllerTest.java
+++ M2M-manager/src/test/java/com/brunobat/m2m/manager/DeviceControllerTest.java
@@ -1,10 +1,14 @@
 package com.brunobat.m2m.manager;
 
 import org.junit.*;
+import org.mockito.Mockito;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Bruno Baptista on 15/12/16.
@@ -46,5 +50,49 @@ public class DeviceControllerTest {
                 "Device: Garage Thermometer reads: 7\n" +
                         "Device: Outside Thermometer reads: 20", deviceController.getDeviceStatus());
     }
+
+    @Test
+    public void testGetDeviceStatus_mock() throws Exception {
+        //setup
+        ResourceManager resourceManager = Mockito.mock(ResourceManager.class);
+        deviceController.setManager(resourceManager);
+
+        // expectations
+        Map<String, String> expectations = new HashMap<>();
+        expectations.put("first", "-1");
+        expectations.put("second", "10");
+        when(resourceManager.getReadingFromAllDevices()).thenReturn(expectations);
+
+        // execute
+        final String deviceStatus = deviceController.getDeviceStatus();
+
+        // validate
+        assertEquals(
+                "Device: first reads: -1\n" +
+                        "Device: second reads: 10\n", deviceStatus);
+    }
+//
+//    @Test
+//    public void testGetDeviceStatus_spy() throws Exception {
+//        //setup
+//        ResourceManager resourceManager = Mockito.mock(ResourceManager.class);
+//        deviceController.setManager(resourceManager);
+//
+//        // expectations
+//        Map<String, String> expectations = new HashMap<>();
+//        expectations.put("first", "-1");
+//        expectations.put("second", "10");
+//        when(resourceManager.getReadingFromAllDevices()).thenReturn(expectations);
+//
+//        // execute
+//        final String deviceStatus = deviceController.getDeviceStatus();
+//
+//        // validate
+//        assertEquals(
+//                "Device: first reads: -1\n" +
+//                        "Device: second reads: 10\n", deviceStatus);
+//    }
+
+
 
 }
