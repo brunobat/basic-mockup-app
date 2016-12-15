67d779365138b3774ec434b7f63376fc5c54298d
Bruno Baptista <brunobat@gmail.com>
2016-12-15 15:48:38 +0000

    test the resource manager

--- M2M-manager/src/test/java/com/brunobat/m2m/manager/DeviceControllerTest.java
+++ M2M-manager/src/test/java/com/brunobat/m2m/manager/DeviceControllerTest.java
@@ -8,7 +8,8 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Bruno Baptista on 15/12/16.
@@ -71,28 +72,58 @@ public class DeviceControllerTest {
                 "Device: first reads: -1\n" +
                         "Device: second reads: 10\n", deviceStatus);
     }
-//
-//    @Test
-//    public void testGetDeviceStatus_spy() throws Exception {
-//        //setup
-//        ResourceManager resourceManager = Mockito.mock(ResourceManager.class);
-//        deviceController.setManager(resourceManager);
-//
-//        // expectations
-//        Map<String, String> expectations = new HashMap<>();
-//        expectations.put("first", "-1");
-//        expectations.put("second", "10");
-//        when(resourceManager.getReadingFromAllDevices()).thenReturn(expectations);
-//
-//        // execute
-//        final String deviceStatus = deviceController.getDeviceStatus();
-//
-//        // validate
-//        assertEquals(
-//                "Device: first reads: -1\n" +
-//                        "Device: second reads: 10\n", deviceStatus);
-//    }
 
+    @Test
+    public void testGetDeviceStatus_withArguments() throws Exception {
+        //setup
+        ResourceManager resourceManager = Mockito.mock(ResourceManager.class);
+        deviceController.setManager(resourceManager);
 
+        // expectations
+        when(resourceManager.getReading("first")).thenReturn("-1");
+
+        // execute
+        final String deviceStatus1 = deviceController.getDeviceStatus("first");
+        // validate
+        assertEquals("Device: first reads: -1\n", deviceStatus1);
+
+        // execute
+        final String deviceStatus2 = deviceController.getDeviceStatus("second");
+        // validate
+        assertEquals("", deviceStatus2);
+    }
+
+    @Test
+    public void testGetDeviceStatus_withArguments_anyString() throws Exception {
+        //setup
+        ResourceManager resourceManager = Mockito.mock(ResourceManager.class);
+        deviceController.setManager(resourceManager);
+
+        // expectations
+        when(resourceManager.getReading(anyString())).thenReturn("10");
+        // execute
+        final String deviceStatus = deviceController.getDeviceStatus("something");
+        // validate
+        assertEquals("Device: something reads: 10\n", deviceStatus);
+    }
+
+    @Test
+    public void testGetDeviceStatus_withArguments_verify() throws Exception {
+        //setup
+        ResourceManager resourceManager = Mockito.mock(ResourceManager.class);
+        deviceController.setManager(resourceManager);
+
+        // execute
+        final String deviceStatus = deviceController.getDeviceStatus("something");
+
+        // validate
+        verify(resourceManager, times(1)).getReading("something");
+        verify(resourceManager, atMost(2)).getReading("something");
+
+        verify(resourceManager, times(0)).getReading("notExpected");
+        verify(resourceManager, never()).getReading("notExpected");
+
+        verify(resourceManager, atLeastOnce()).getReading(anyString());
+    }
 
 }
